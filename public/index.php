053779854923bf04c275a46620f6b6e9124ffbe4
Shaun Alberts <shaunalberts@gmail.com>
2017-08-25 21:16:07 +0200

    Demo stale entity issue

--- public/index.php
+++ public/index.php
@@ -13,9 +13,23 @@ $entityManager = EntityManager::create([
 	'dbname'   => 'stale-test-tmp',
 ], $config);
 
+// Fetch user id=2 from the DB
+// It looks like this result will get cached and override the result when this entity is retrieved in the transaction below :(
+$query = $entityManager->createQuery('SELECT foo FROM \Demo\Models\UserModel foo WHERE foo.id = :id');
+$query->setParameter('id', 2);
+$user1 = $query->getOneOrNullResult();
 
-$user = new \Demo\Models\UserModel();
+// Open a db transaction
+$entityManager->getConnection()->beginTransaction();
+
+// Select user id=2 FOR UPDATE. This will wait to get a lock on the record
+$query = $entityManager->createQuery('SELECT foo FROM \Demo\Models\UserModel foo WHERE foo.id = :id');
+$query->setParameter('id', 2);
+$query->setLockMode(\Doctrine\DBAL\LockMode::PESSIMISTIC_WRITE);
+$user = $query->getOneOrNullResult();
+
 print('<pre>');
 \Doctrine\Common\Util\Debug::dump($user);
-exit;
+
+$entityManager->getConnection()->rollBack();
 
