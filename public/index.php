dccb6f2738ce54642ad39e658c732e4317f5d97a
Shaun Alberts <shaunalberts@gmail.com>
2017-08-25 21:40:19 +0200

    Update id=1 because reasons

--- public/index.php
+++ public/index.php
@@ -15,8 +15,8 @@ $entityManager = EntityManager::create([
 
 // Fetch user id=2 from the DB
 // It looks like this result will get cached and override the result when this entity is retrieved in the transaction below :(
-$query = $entityManager->createQuery('SELECT foo FROM \Demo\Models\UserModel foo WHERE foo.id = :id');
-$query->setParameter('id', 2);
+$query = $entityManager->createQuery('SELECT User FROM \Demo\Models\UserModel User WHERE User.id = :id');
+$query->setParameter('id', 1);
 $user1 = $query->getOneOrNullResult();
 
 // Open a db transaction
@@ -24,7 +24,7 @@ $entityManager->getConnection()->beginTransaction();
 
 // Select user id=2 FOR UPDATE. This will wait to get a lock on the record
 $query = $entityManager->createQuery('SELECT foo FROM \Demo\Models\UserModel foo WHERE foo.id = :id');
-$query->setParameter('id', 2);
+$query->setParameter('id', 1);
 $query->setLockMode(\Doctrine\DBAL\LockMode::PESSIMISTIC_WRITE);
 $user = $query->getOneOrNullResult();
 
